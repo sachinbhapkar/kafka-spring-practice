9a6b262765871923bb2ec5c42ef70febe65beb7f
sachin <31815217+sachinbhapkar@users.noreply.github.com>
2022-12-04 19:35:12 +0530

    Add files via upload

---
+++ src/test/java/org/e4e5/kafkaspring/KafkaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.e4e5.kafkaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
